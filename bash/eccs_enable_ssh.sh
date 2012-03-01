9cb45b1156a5748456fdfa3f04cc892b9ba71b90
Patrick Gallagher <patgmac+git@gmail.com>
2012-03-01 15:09:40 -0500

    Modified enable ssh script

--- bash/eccs_enable_ssh.sh
+++ bash/eccs_enable_ssh.sh
@@ -7,12 +7,19 @@
 
 ## Change Log
 # 2/16/2012 - Initial script
+# 3/1/2012 - Added logic to create ssh group only if it didn't already exist. 
 
 # Enable ssh
 systemsetup -setremotelogin on
 
 # Create the com.apple.access_ssh group
-dseditgroup -o create -q com.apple.access_ssh
+sshGroup=`dseditgroup -o checkmember com.apple.access_ssh`
+if [[ "$sshGroup" == "Group not found." ]]; then # This is not working yet
+	echo "Creating ssh access group"
+	dseditgroup -o create -q com.apple.access_ssh
+fi
 
 # Add the admin group to com.apple.access_ssh
 dseditgroup -o edit -a admin -t group com.apple.access_ssh
+
+exit 0
