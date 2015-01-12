5c6537ed9059697e9c46df31805768391b0260cc
Patrick Gallagher <patgmac@gmail.com>
2015-01-12 09:16:25 -0500

    Updated ssh script

--- bash/eccs_enable_ssh.sh
+++ bash/eccs_enable_ssh.sh
@@ -3,16 +3,17 @@
 # eccs_config_ssh
 # Patrick Gallagher
 # Created 2/16/2012
-# Modified 3/27/2012
+# Modified 6/17/2014
 
 ## Change Log
 # 2/16/2012 - Initial script
 # 3/1/2012 - Added logic to create ssh group only if it didn't already exist. 
 # 3/5/2012 - Fixed logic with help of @gregneagle and @tvsutton
 # 3/27/2012 - Changed how ssh is enabled. No longer using deprecated systemsetup. 
+# 6/17/2014 - Back to using systemsetup since Mavericks doesn't like plistbuddy. 
 
 # Enable ssh
-/usr/libexec/PlistBuddy -c "Delete Disabled" "/System/Library/LaunchDaemons/ssh.plist"
+systemsetup -setremotelogin on
 launchctl load -w /System/Library/LaunchDaemons/ssh.plist
 
 # Create the com.apple.access_ssh group
